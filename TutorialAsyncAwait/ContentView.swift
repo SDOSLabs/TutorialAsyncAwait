fcbf70ddddf808fbcbec6bcc8b541a427907be68
Rafael Fernández <rafael.fernandez@sdos.es>
2021-09-16 11:09:47 +0200

    Initial Commit

---
+++ TutorialAsyncAwait/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TutorialAsyncAwait
+//
+//  Created by Rafael Fernandez Alvarez on 16/9/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TutorialAsyncAwait/TutorialAsyncAwaitApp.swift
@@ -0,0 +1,17 @@
+//
+//  TutorialAsyncAwaitApp.swift
+//  TutorialAsyncAwait
+//
+//  Created by Rafael Fernandez Alvarez on 16/9/21.
+//
+
+import SwiftUI
+
+@main
+struct TutorialAsyncAwaitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
